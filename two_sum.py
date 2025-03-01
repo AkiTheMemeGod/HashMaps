7dc7e96582441539096ec0964571aa21e2ebf958
AkiTheMemeGod <k.akashkumar@gmail.com>
2025-03-01 18:58:14 +0530

    Add sample input and target to two_sum function

--- two_sum.py
+++ two_sum.py
@@ -1,6 +1,7 @@
 from collections import defaultdict
 def two_sum():
-
+    nums = [2, 4, 7, 11, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100]
+    target = 155
     x = {}
     for i, n in enumerate(nums):
         diff = target - n
